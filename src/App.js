9d7618e3f623433a0192b61d472a7bdd1b98d4d2
Ahmed Alhashimi <alhashimi346@gmail.com>
2022-06-08 21:46:14 -0600

    initial commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  // Link
+} from "react-router-dom";
+
+import Home from './pages/Home'
+
+function App() {
+  return (
+    <div className="App">
+      <div className="App-header">
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/Home.js
@@ -0,0 +1,17 @@
+import {Grid, Button, Typography} from '@material-ui/core';
+import Icon from '@mdi/react'
+import { Link } from "react-router-dom";
+
+function Home() {
+
+    return (
+        <div>
+            <Typography style={{margin:30}} variant="h2">
+            React APP
+            </Typography>
+        </div>
+    );
+  }
+  
+  export default Home;
+  
